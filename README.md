5fe2e7dd73ea3963b18435f40032dc74c9d2cdef
Cappy Ishihara <cappy@cappuchino.xyz>
2026-03-26 10:18:04 +0700

    implement vmm code, stub test path (no API yet)

--- README.md
+++ README.md
@@ -4,6 +4,23 @@
 
 Stack Virtualization Platform - Powered by Cloud Hypervisor
 
+Odorobo^(robot dance)^ is a hypervisor platform built on top of 
+[Cloud Hypervisor](https://www.cloudhypervisor.org/), a lightweight 
+VMM built on rust-vmm, with a focus on providing lightweight,
+stateful, and portable virtual machines.
+
+VM configuration is persisted by the gateway and portable across nodes;
+disk images can be backed by distributed storage for full portability, or kept node-local for simplicity.
+
+## Components
+
+- **Agent**: A lightweight agent that handles VM lifecycle management, including creation, deletion, and state management.
+- **Gateway**: A central controller that orchestrates the deployment and management of VMs, reconciling and handling migration across nodes (unimplemented).
+- **CLI**: Debugging and management command-line interface for interacting with the platform, not intended for end-users but useful for developers and operators (unimplemented).
+
+The Gateway orchestrates across nodes;
+Agents run on each node and manage Cloud Hypervisor instances directly via systemd.
+
 ## Usage
 
 TODO
--- odorobo-agent/src/main.rs
+++ odorobo-agent/src/main.rs
@@ -1,11 +1,47 @@
+
 mod state;
+use cloud_hypervisor_client::{
+    apis::DefaultApi,
+    models::{CpusConfig, VmConfig},
+};
 use stable_eyre::Result;
-use std::fs::{self};
-
-fn main() -> Result<()> {
+use state::VMStateManager;
+#[tokio::main]
+async fn main() -> Result<()> {
     stable_eyre::install()?;
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
 
-    println!("Hello, world!");
+    state::init()?;
+    tracing::info!("Starting odorobo-agent...");
+
+    // println!("Hello, world!");
+
+    let testvm = state::VMInstance::create_instance("testvm", None).await?;
+
+    let list = state::VMInstance::list()?;
+
+    tracing::info!("VM Instances: {list:#?}");
+
+    // let info = testvm
+    //     .apply_config(&VmConfig {
+    //         cpus: Some(CpusConfig {
+    //             boot_vcpus: 2,
+    //             max_vcpus: 4,
+    //             topology: None,
+    //             ..Default::default()
+    //         }),
+    //         ..Default::default()
+    //     })
+    //     .await?;
+    // tracing::info!(?info);
+
+    let vminfo = testvm.info().await?;
+
+    testvm.vmm_ping_get().await?;
+
+    tracing::info!(?vminfo);
 
     Ok(())
 }
