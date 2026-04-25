fa0add4428feb5388c0a6d9ad0ae29e318c49698
Caleb Jones <caleb@calebgj.io>
2026-04-25 13:17:38 -0500

    add caching of vm config in agent actor and report resource usage data in AgentStatus
    Signed-off-by: Caleb Jones <caleb@calebgj.io>

--- odorobo-shared/src/messages/agent.rs
+++ odorobo-shared/src/messages/agent.rs
@@ -10,7 +10,11 @@ pub struct GetAgentStatus;
 #[derive(Serialize, Deserialize, Reply, Debug, Clone)]
 pub struct AgentStatus {
     pub hostname: String,
+    // todo: do we want to worry about things like CCX on epic chips? likely not necessary day 1 given we don't have epics.
+    /// Total number of vcpus before over-provisionment.
     pub vcpus: u32,
     pub ram: ByteSize,
+    pub used_vcpus: u32,
+    pub used_ram: ByteSize,
     pub vms: Vec<Ulid>
 }
