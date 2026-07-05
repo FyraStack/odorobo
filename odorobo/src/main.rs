e7f08f590db00b3d4646ba7fba686a37097a4c32
madonuko <madonuko@outlook.com>
2026-07-05 18:11:17 +0800

    feat: watch for termsigs

--- odorobo/src/main.rs
+++ odorobo/src/main.rs
@@ -9,6 +9,8 @@ pub mod types;
 mod utils;
 
 use std::fs;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 use clap::Parser;
 use kameo::actor::Spawn;
@@ -21,9 +23,7 @@ use crate::config::Config;
 use crate::utils::actor_names::{HTTP_API_SERVER, SCHEDULER};
 use crate::utils::{actor_names::AGENT, connect_to_swarm, init};
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let _ = utils::lockfile::init_lockfile();
+fn main() -> Result<()> {
     let cli_config = config::CliConfig::parse();
     // TODO: ask infra team where they want this on the box
     let config: Config = if let Ok(file) = fs::File::open("config.json") {
@@ -33,7 +33,32 @@ async fn main() -> Result<()> {
     };
 
     init(Some("odorobo"))?;
+    let term = utils::lockfile::register_termsigs()?;
+    let _lock = utils::lockfile::init_lockfile()
+        .map_err(|e| stable_eyre::eyre::eyre!("cannot init lockfile").wrap_err(e))?;
 
+    mainloop(term, cli_config, config)
+}
+
+fn mainloop(term: Arc<AtomicBool>, cli_config: config::CliConfig, config: Config) -> Result<()> {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("can't build tokio");
+    let handle = runtime.spawn(inner_main(cli_config, config));
+
+    loop {
+        if handle.is_finished() {
+            break runtime.block_on(handle).expect("cannot join main thread");
+        }
+        if term.load(Ordering::Relaxed) {
+            handle.abort();
+            stable_eyre::eyre::bail!("Exit due to termination signal");
+        }
+    }
+}
+
+async fn inner_main(cli_config: config::CliConfig, config: Config) -> Result<()> {
     tracing::info!(?config, "Starting odorobo");
 
     let local_peer_id = connect_to_swarm().unwrap();
--- odorobo/src/utils/lockfile.rs
+++ odorobo/src/utils/lockfile.rs
@@ -1,12 +1,13 @@
-use tokio::io::AsyncWriteExt;
+use std::io::Write;
 
 const LOCKFILE: &str = "/var/lock/odorobo.lock";
 
 // `Option` needed since `std::mem::take` requires `impl Default`
-pub struct Lockfile(Option<tokio::fs::File>);
+pub struct Lockfile(Option<std::fs::File>);
 
 impl Drop for Lockfile {
     fn drop(&mut self) {
+        tracing::debug!("dropping Lockfile");
         std::mem::drop(std::mem::take(&mut self.0).expect("None in Lockfile"));
         _ = std::fs::remove_file(LOCKFILE)
             .inspect_err(|e| tracing::warn!(?LOCKFILE, ?e, "cannot remove lockfile"));
@@ -16,12 +17,26 @@ impl Drop for Lockfile {
 /// Create an odorobo lockfile
 ///
 /// See #30, only 1 instance of odorobo should run.
-pub async fn init_lockfile() -> Result<Lockfile, String> {
-    let mut f = tokio::fs::File::create_new(LOCKFILE)
-        .await
+pub fn init_lockfile() -> Result<Lockfile, String> {
+    tracing::trace!("creating lockfile at {LOCKFILE}");
+    let mut f = std::fs::File::create_new(LOCKFILE)
         .map_err(|e| format!("Cannot create lockfile at {LOCKFILE}: {e:?}"))?;
     f.write_all(&std::process::id().to_ne_bytes())
-        .await
         .map_err(|e| format!("cannot write to {LOCKFILE}: {e:?}"))?;
+    f.flush()
+        .map_err(|e| format!("cannot flush {LOCKFILE}: {e:?}"))?;
     Ok(Lockfile(Some(f)))
+}
+
+/// Register termination signals to watch
+///
+/// We need to tidy up lockfiles before exiting. Watching these signals can give us a chance to
+/// actually clean things up.
+pub fn register_termsigs() -> std::io::Result<std::sync::Arc<std::sync::atomic::AtomicBool>> {
+    let term = std::sync::Arc::new(std::sync::atomic::AtomicBool::new(false));
+    for sig in signal_hook::consts::TERM_SIGNALS {
+        signal_hook::flag::register_conditional_shutdown(*sig, 1, std::sync::Arc::clone(&term))?;
+        signal_hook::flag::register(*sig, std::sync::Arc::clone(&term))?;
+    }
+    Ok(term)
 }
