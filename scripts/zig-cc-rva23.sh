ecf5f67efca88acfe1991a633de392b3061b3b9f
Cypress Reed <cypress@fyralabs.com>
2026-08-01 17:50:38 -0600

    fix thing

--- scripts/zig-cc-rva23.sh
+++ scripts/zig-cc-rva23.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 args=()
 for arg in "$@"; do
 	case "$arg" in
-	--target=riscv64-unknown-linux-gnu)
+	--target=riscv64-unknown-linux-gnu|--target=riscv64a23-unknown-linux-gnu)
 		;;
 	*)
 		args+=("$arg")
